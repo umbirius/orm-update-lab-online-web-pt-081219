1909227747977c82f123090674425e3ec2ee50c3
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-18 19:33:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,10 +52,10 @@ class Student
   end 
   
   def self.new_from_db(row)
-    new_student = self.new 
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    new_student = Student.new(name, grade, id)
   end 
   
   def self.find_by_name
