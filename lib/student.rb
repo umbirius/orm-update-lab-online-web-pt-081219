801eceae9659890eb348a9e01921d461c3cdaa0c
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-18 18:56:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,23 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  attr_accessor :name, :grade 
+  attr_reader :id
 
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+  end 
+
+  def self.create_table
+    
+    sql = <<-SQL 
+      CREATE TABLE students(
+      id INTEGER PRIMARY KEY,
+      name TEXT, 
+      grade INTEGER)
+    SQL
+    
+    
 
 end
