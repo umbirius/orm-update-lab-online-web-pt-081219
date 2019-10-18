e8a79ff5059348dff2b00cbe0395c5a15fa4840f
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-18 19:08:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,8 +50,25 @@ class Student
     student
   end 
   
-  def self.new_from_db
-    
+  def self.new_from_db(row)
+    new_student = self.new 
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end 
+  
+  def self.find_by_name
+    sql = <<-SQL 
+      SELECT * 
+      FROM student 
+      WHERE name = ? 
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql, name).map do |row|
+      new_from_db(row)
+    end.first
+      
 end 
 
